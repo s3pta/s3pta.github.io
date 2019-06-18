6605b9bf8a9fc376ebf92e872f775497f475ef39
franciscofofo <39025735+franciscofofo@users.noreply.github.com>
2019-06-18 19:09:53 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 $(document).ready(() => {
-  $('.TitlePic')
+  $(window).on('scroll', () => {
+    const scrollTop = $(window).scrollTop
+    const scrollLeft = $(window).scrollLeft
+    $('.BannerPicture').css({
+      'transform': `translate(${scrollLeft / 2}px, ${scrollTop / 2}px)`
+    })
+  })
 })
