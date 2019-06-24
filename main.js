0469745c4093b37e78cbfd6dbaca6109f68b1999
franciscofofo <carricokiko2006@gmail.com>
2019-06-24 10:59:45 +0200

    Look improvements

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $(document).ready(() => {
     const scrollTop = $(this).scrollTop()
     const scrollLeft = $(this).scrollLeft()
     $('.BannerPicture').css({
+      'transform': `translate(${scrollLeft / 1.3}px, ${scrollTop / 1.3}px)`
+    })
+    $('.Title h1, .Title h2').css({
       'transform': `translate(${scrollLeft / 2}px, ${scrollTop / 2}px)`
     })
     if (scrollTop > 20) {
